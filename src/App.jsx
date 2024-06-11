5a1f57f51abdf7476863bf2f7631f417f7c8bf81
YashrajKamble <yashrajkamble7727@gmail.com>
2024-06-11 18:44:15 +0530

    adding function in app is done

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
@@ -15,6 +15,27 @@ function App() {
     setTodos((prev) =>
       prev.map((prevTodo) => (prevTodo.id === id ? todo : prevTodo))
     );
+  };
+
+  const deleteTodo = (id) => {
+    setTodos((prev) => prev.filter((todo) => todo.id !== id));
+  };
+
+  const toggleComplete = (id) => {
+    setTodos((prev) =>
+      prev.map((prevTodo) =>
+        prevTodo === id ? { ...prev, completed: !prevTodo.completed } : prevTodo
+      )
+    );
+
+    useEffect(() => {
+      const todos = JSON.parse(localStorage.getItem("todos"));
+      if (todos && todos.length > 0) setTodos(todos);
+    }, []);
+
+    useEffect(() => {
+      localStorage.setItem("todos", JSON.stringify(todos));
+    }, []);
   };
 
   return (
